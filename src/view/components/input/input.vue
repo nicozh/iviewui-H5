3cb965895fba5fc5b66fb7fb1c35294c8747bd98
zhanghong <zhanghong@ceemoo.com>
2018-11-14 17:04:08 +0800

    add input

---
+++ src/view/components/input/input.vue
@@ -0,0 +1,195 @@
+<template>
+    <div>
+        <div :class="[iClass, 'i-cell', 'i-input', error ? 'i-input-error' : '', mode === 'wrapped' ? 'i-input-wrapped' : '']">
+            <div v-if="title" class="i-cell-hd i-input-title">{{ title }}</div>
+            <textarea v-if="type === 'textarea'" auto-height :disabled="disabled" :autofocus="autofocus" :value="value"
+                :placeholder="placeholder" :maxlength="maxlength" :class="['i-input-input', 'i-cell-bd', right ? 'i-input-input-right' : '']"
+                placeholder-class="i-input-placeholder" @input="handleInputChange" @focus="handleInputFocus" @blur="handleInputBlur"></textarea>
+            <input v-else :type="type" :disabled="disabled" :autofocus="autofocus" :value="value" :placeholder="placeholder"
+                :maxlength="maxlength" :class="['i-input-input', 'i-cell-bd', right ? 'i-input-input-right' : '']"
+                placeholder-class="i-input-placeholder" @input="handleInputChange" @focus="handleInputFocus" @blur="handleInputBlur">
+        </div>
+    </div>
+</template>
+<script>
+export default {
+  props: {
+    value: [String, Number],
+    iClass: String,
+    title: {
+      type: String
+    },
+    // text || textarea || password || number
+    type: {
+      type: String,
+      default: "text"
+    },
+    disabled: {
+      type: Boolean,
+      default: false
+    },
+    placeholder: {
+      type: String,
+      default: ""
+    },
+    autofocus: {
+      type: Boolean,
+      default: false
+    },
+    mode: {
+      type: String,
+      default: "normal"
+    },
+    right: {
+      type: Boolean,
+      default: false
+    },
+    error: {
+      type: Boolean,
+      default: false
+    },
+    maxlength: {
+      type: Number
+    }
+  },
+  data() {
+    return {};
+    // value: ''
+  },
+  watch: {
+    value() {
+      this.$nextTick(this.adjustSize);
+    }
+  },
+  mounted() {
+    this.$nextTick(this.adjustSize);
+  },
+  methods: {
+    isObj(x) {
+      const type = typeof x;
+      return x !== null && (type === "object" || type === "function");
+    },
+    handleInputChange(event) {
+      // const {
+      //     detail = {}
+      // } = event;
+      // const {
+      //     value = ''
+      // } = detail;
+      // this.setData({
+      //     value
+      // });
+
+      // this.triggerEvent('change', event);
+      this.$emit("change", this.value);
+    },
+
+    handleInputFocus(event) {
+      // this.triggerEvent('focus', event);
+      this.$emit("focus", event);
+    },
+
+    handleInputBlur(event) {
+      // this.triggerEvent('blur', event);
+      this.$emit("blur", event);
+    },
+    adjustSize() {
+      const { input } = this.$refs;
+      if (!(this.type === "textarea" && this.autosize) || !input) {
+        return;
+      }
+      input.style.height = "auto";
+      let height = input.scrollHeight;
+      if (this.isObj(this.autosize)) {
+        const { maxHeight, minHeight } = this.autosize;
+        if (maxHeight) {
+          height = Math.min(height, maxHeight);
+        }
+        if (minHeight) {
+          height = Math.max(height, minHeight);
+        }
+      }
+      if (height) {
+        input.style.height = height + "px";
+      }
+    }
+  }
+};
+</script>
+
+<style lang="less">
+@import "../styles/_base.less";
+@import "../styles/_mixins.less";
+@import "../cell/index.less";
+
+.i-input {
+  padding: 7px 15px;
+  color: @text-color;
+
+  &-wrapped {
+    margin: 10px 15px;
+    background-color: #fff;
+
+    &::after {
+      left: 0;
+      border-width: 1px;
+      border-radius: 4px;
+    }
+  }
+
+  &-error {
+    color: @error-color;
+  }
+
+  &-title {
+    color: @text-color;
+    min-width: 65px;
+    padding-right: 10px;
+  }
+
+  &-input {
+    flex: 1;
+    line-height: 1.6;
+    padding: 4px 0;
+    min-height: 22px;
+    height: auto;
+    font-size: 14px;
+    border: 0;
+    margin: 0;
+    // padding: 0;
+    width: 100%;
+    resize: none;
+    display: block;
+    box-sizing: border-box;
+    background-color: transparent;
+    outline: none;
+
+    &::-webkit-outer-spin-button,
+    &::-webkit-inner-spin-button {
+      -webkit-appearance: none;
+    }
+
+    // input[type="number"] {
+    //     -moz-appearance: textfield;
+    // }
+  }
+
+  &-placeholder {
+    font-size: @size-font-base;
+  }
+
+  &-input-right {
+    text-align: right;
+  }
+}
+
+/* 圆角输入框，强制展示边框 */
+.i-input.i-input-wrapped::after {
+  display: block;
+}
+
+/* 圆角输入框出现错误时，将边框也置红 */
+.i-input-wrapped.i-input-error::after {
+  border-color: @error-color;
+}
+</style>
