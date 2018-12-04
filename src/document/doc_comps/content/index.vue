3b2c48bbee8961859c385f1c03c34f346adb6924
zhanghong <zhanghong@ceemoo.com>
2018-12-04 18:07:30 +0800

    添加文档样式

--- src/document/doc_comps/content/index.vue
+++ src/document/doc_comps/content/index.vue
@@ -6,10 +6,34 @@
 
 <style lang="less">
 .comp-content {
-  padding: 30px 0 30px 30px;
+  padding: 30px 20px 30px 30px;
   height: 100%;
   box-sizing: border-box;
   overflow: auto;
+  & table {
+    font-family: Consolas, Menlo, Courier, monospace;
+    font-size: 12px;
+    border-collapse: collapse;
+    border-spacing: 0;
+    empty-cells: show;
+    border: 1px solid #e9e9e9;
+    width: 100%;
+    margin:0 0 24px 0;
+    & th {
+      border: 1px solid #e9e9e9;
+      padding: 8px 16px;
+      text-align: left;
+      background: #f7f7f7;
+      white-space: nowrap;
+      color: #5c6b77;
+      font-weight: 600;
+    }
+    & td {
+      border: 1px solid #e9e9e9;
+      padding: 8px 16px;
+      text-align: left;
+    }
+  }
 }
 </style>
 
